479ef5835106ed722b6a9f2bf27c4770124d51e5
JesusGonzalezPerez <jesusgpeligros@gmail.com>
2021-08-21 01:58:27 +0200

    Final commit
    Full remake project

--- Pelis/js/index.js
+++ Pelis/js/index.js
@@ -1,142 +1,159 @@
-var pag = 1;
-var id = 1;
-$(window).scroll(peticionAJAX);
+let url = "https://www.omdbapi.com/?"
+let apikey = "apikey=3695b132"
+let pagina = 1;
 
-function peticionAJAX() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            maquetaRespuesta(JSON.parse(this.responseText));
-        }
-    };
+// apikey=56686ffa
+// apikey=3695b132
+// apikey=3aa482bb
 
-    var buscar = document.getElementById('textfield').value;
-    var nombre = buscar.split(" ");
-    var busqueda = "";
+/**
+ * Empty div, restart variables to default, and search the new petition
+ * 
+ * @author Jesús González <jesusgpeligros@gmail.com>
+ */
+function restart() {
+    $('#div').empty();
+    pagina = 1;
+    search();
+}
+$("#buscar").on("click", () => restart());
+
+/**
+ * Perform the search
+ * 
+ * @author Jesús González <jesusgpeligros@gmail.com>
+ */
+function search() {
+    let nombreBuscar = $("#titulo").val();
+    let nombre = nombreBuscar.split(" ");
+    let buscar = "";
+    // Split the words with "+"
     for (x = 0; x < nombre.length; x++) {
         if (x == 0 && nombre.length > 1) {
-            busqueda += nombre[x] + "+";
+            buscar += nombre[x] + "+";
         } else if (x < nombre.length - 1) {
-            busqueda += nombre[x] + "+";
+            buscar += nombre[x] + "+";
         } else {
-            busqueda += nombre[x];
+            buscar += nombre[x];
         }
     }
-
-    xhttp.open("GET", "https://www.omdbapi.com/?s=" + busqueda + "&page=" + pag + "&apikey=56686ffa", true);
-
-    // apikey=56686ffa
-    // apikey=3695b132
-    // apikey=3aa482bb
-
-    xhttp.send();
-    pag += 1;
+    // URL
+    let urlSearch = url + "s=" + buscar + "&page=" + pagina + "&" + apikey;
+    // Make the AJAX request
+    $.ajax({
+        url: urlSearch,
+        success: function (response) {
+            layoutObjects(response);
+        }
+    })
+    // Increase the variable
+    pagina++;
 }
 
-
-function maquetaRespuesta(objetoJSON) {
-    var div = document.getElementById('div');
-    for (var i = 0; i < objetoJSON.Search.length; i++) {
-
-        var card = document.createElement('div');
+/**
+ * Mock up the objects it receives from the request to the api
+ * 
+ * @param {JSON} response 
+ * 
+ * @author Jesús González <jesusgpeligros@gmail.com>
+ */
+function layoutObjects(response) {
+    // Main div
+    $('#div').attr('class', 'd-flex justify-content-around');
+    let div = document.getElementById('div');
+    
+    // Loop all objects of response
+    $.each(response.Search, function (index,objetoJSON) {
+        // Object div
+        let card = document.createElement('div');
+        $(card).on('click', () => datosPelicula(card.id))
+        card.id = objetoJSON.imdbID;
         card.setAttribute("class", "card");
         card.setAttribute("data-toggle", "modal");
         card.setAttribute("data-target", "#info");
-        card.setAttribute("id", objetoJSON.Search[i].imdbID);
-        card.setAttribute("onclick", "peticionPeli(event)");
-
-        var img = document.createElement('img');
-        if (objetoJSON.Search[i].Poster == "N/A") {
+        card.setAttribute("id", objetoJSON.imdbID);
+        // Set default imagen if the object does not have
+        let img = document.createElement('img');
+        if (objetoJSON.Poster == "N/A") {
             img.src = "https://i.pinimg.com/originals/51/9c/18/519c18a68160ffb6d5aa5d92cd1e3d59.jpg";
             card.append(img);
         } else {
-            img.src = "" + objetoJSON.Search[i].Poster + "";
+            img.src = "" + objetoJSON.Poster + "";
             card.append(img);
         }
-
-        var texto = document.createElement('div');
+        // Title div
+        let Divtexto = document.createElement('div');
+        Divtexto.setAttribute("class", "Divtexto");
+        Divtexto.style.display = "flex";
+        Divtexto.style.alignItems = "center";
+        // Title
+        let texto = document.createElement('p');
+        texto.style.margin = "0";
         texto.setAttribute("class", "texto");
-        texto.textContent = objetoJSON.Search[i].Title;
-        card.append(texto);
+        texto.textContent = objetoJSON.Title;
+        Divtexto.append(texto);
+        card.append(Divtexto);
 
         div.append(card);
+    })
+    // Button to load more elements
+    if(response.Response!="False"){
+        let divBtn = document.createElement("div");
+        divBtn.classList.add("divMore","d-flex","justify-content-center","my-2");
+        divBtn.style.width = "100vw";
+        let btn = document.createElement("button");
+        btn.classList.add("btn","btn-outline-success","btn-lg","moreBtn");
+        btn.textContent = "More";
+        btn.addEventListener("click", () => {
+            search();
+            $(".moreBtn").css("display","none");
+        });
 
-        id++;
+        divBtn.append(btn);
+        div.append(divBtn);
     }
 }
 
-
-
-
-
-
-function peticionPeli(event) {
-    var id = event.target.id;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            maquetaModal(JSON.parse(this.responseText));
+/**
+ * Request to the api to receive the data with the object id
+ * 
+ * @param {String} id 
+ * 
+ * @author Jesús González <jesusgpeligros@gmail.com>
+ */
+function datosPelicula(id) {
+    let id1 = "i=" + id;
+    let urlDatos = url + id1 + "&" + apikey;
+    $.ajax({
+        url: urlDatos,
+        success: function (response) {
+            colocarModal(response);
         }
-    };
-    xhttp.open("GET", "https://www.omdbapi.com/?i=" + id + "&apikey=56686ffa", true);
-    xhttp.send();
+    })
 
 }
 
-function maquetaModal(peli) {
-    var imgModal = document.getElementById('poster');
-    var datosModal = document.getElementById('datos');
-
-    while (imgModal.firstChild) {
-        imgModal.removeChild(imgModal.firstChild);
-    }
-
-    datosModal.innerHTML = "";
-
-    var poster = document.createElement('img');
-    if (peli.Poster == "N/A") {
-        poster.src = "https://i.pinimg.com/originals/51/9c/18/519c18a68160ffb6d5aa5d92cd1e3d59.jpg";
+/**
+ * Fill the modal with the object data
+ * 
+ * @param {JSON} response 
+ * 
+ * @author Jesús González <jesusgpeligros@gmail.com>
+ */
+function colocarModal(response) {
+    $('.modal-title').text(response.Title);
+    // Set default imagen if the object does not have
+    if (response.Poster == 'N/A') {
+        $('#imagen').attr('src', 'https://i.pinimg.com/originals/51/9c/18/519c18a68160ffb6d5aa5d92cd1e3d59.jpg');
     } else {
-        poster.src = peli.Poster;
+        $('#imagen').attr('src', response.Poster);
     }
-
-
-    var titulo = document.getElementById('modalTitle');
-    titulo.innerHTML = peli.Title;
-
-   var year = document.createElement('p');
-   year.innerHTML =  "Año: "+  peli.Year;
-
-    var genre = document.createElement('p');
-    genre.innerText = "Género : " + peli.Type;
-
-    var idioma = document.createElement('p');
-    idioma.innerText = "Idioma : " + peli.Language;
-
-    var value = document.createElement('p');
-    value.innerText = "Value : " + peli.imdbRating;
-
-    var plot = document.createElement('p');
-    plot.innerText = "Resumen : " + peli.Plot;
-
-    imgModal.append(poster);
-    datosModal.append(year);
-    datosModal.append(genre);
-    datosModal.append(idioma);
-    datosModal.append(plot);
-    datosModal.append(value);
-
+    
+    $('#genre').text(response.Genre)
+    $('#released').text(response.Released)
+    $('#director').text(response.Director)
+    $('#writer').text(response.Writer)
+    $('#actors').text(response.Actors)
+    $('#plot').text(response.Plot)
+    $('#rating').text(response.imdbRating)
 }
-
-
-
-window.onload = function () {
-    peticionAJAX();
-    // maquetaModal();
-    setInterval(function () {
-        if ((window.scrollY + window.innerHeight) >= (window.innerHeight + 150)) {
-            peticionAJAX();
-        }
-    }, 1000);
-
-};
